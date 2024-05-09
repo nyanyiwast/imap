ce46ee66c42b10075399df3e0ea63ed9629038a5
Sedrick Takunda Nyanyiwa <nyanyiwast@gmail.com>
2024-05-09 18:39:49 +0200

    added a coming soon page

---
+++ src/pages/error-page/coming-soon.jsx
@@ -0,0 +1,11 @@
+export default function ComingSoonPage() {
+
+  return (
+    <div className="flex items-center justify-center h-screen border">
+    <div id="error-page" className="mx-auto justify-center text-center">
+      <h1 className="text-3xl font-bold text-red-400 pb-10">Hey there! This Page Is Coming Soon</h1>
+      <p className="text-lg pb-10">Sorry about that!</p>
+    </div>
+    </div>
+  );
+}
